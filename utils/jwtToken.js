d9a2871d66d05341cd8e59f01bc178d1478d1710
Jayu <jaivikgoswami780@gmail.com>
2022-10-22 10:17:44 +0530

    cookie issue resolved

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,7 +9,8 @@ const sendToken = (user, statuscode, res, next) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: 'none'
     }
     res.cookie("token", token, options).status(statuscode).json({
         success: true,
