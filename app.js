952b0b4ef36fdda38537e7641361ce2b768e0d42
Jayu <jaivikgoswami780@gmail.com>
2023-04-20 10:06:59 +0530

    CORS issue continue

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config({path: "config/config.env"});
 app.use(express.json());
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL,
+    origin: "https://money-trackerr.netlify.app/",
 }))
 app.use(cookieParser());
 
